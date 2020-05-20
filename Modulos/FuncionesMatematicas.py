d330e0134b3c6eed53e1fcc24b16fac7a28fc61d
NicoDemaria <63611798+NicoDemaria@users.noreply.github.com>
2020-05-20 12:25:56 -0300

    Add files via upload

---
+++ Modulos/FuncionesMatematicas.py
@@ -0,0 +1,8 @@
+def sumar(op1,op2):
+	print("El resultad de la suma es:", op1+op2)
+
+def restar(op1,op2):
+	print("El resultad de la resta es:", op1-op2)
+
+def multiplicar(op1,op2):
+	print("El resultad de la x  es:", op1*op2)
---
+++ Modulos/Prueba Funciones.py
@@ -0,0 +1,7 @@
+ #import FuncionesMatematicas
+
+from FuncionesMatematicas import *
+
+sumar(7,5) 
+restar(7,5) 
+multiplicar(5,5)
