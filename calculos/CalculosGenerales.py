13506584e048f4fe29f0d9536c87bbf228f3f7a4
NicoDemaria <63611798+NicoDemaria@users.noreply.github.com>
2020-05-20 12:28:18 -0300

    Add files via upload

---
+++ calculos/CalculosGenerales.py
@@ -0,0 +1,17 @@
+def sumar(op1,op2):
+    print("El resultado de la suma es:", op1+op2)
+
+def restar(op1,op2):
+    print("El resultado de la resta es:", op1-op2)
+
+def multiplicar(op1,op2):
+    print("El resultado de la x es:", op1*op2)
+
+def division(dividento,divisor):
+    print("El resultado de la division es:", dividento/divisor) 
+
+def potencia(base,exponente):
+    print("El resultado de la potencia es:", base**exponente)
+
+def redondear(numero):
+    print("El resultado de la redondacion es:",  round(numero))
